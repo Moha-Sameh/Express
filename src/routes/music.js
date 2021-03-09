4020911fb3fbdb390435934f781c39bdb6a5f83d
Moha-Sameh <ms.slama01@hotmail.com>
2021-03-09 18:25:41 +0200

    Update music with express

--- src/routes/music.js
+++ src/routes/music.js
@@ -3,6 +3,8 @@ const { musicDelete, musicList, musicCreater } = require("../controller/music");
 
 const router = express.Router();
 
+router.use(express.json());
+
 // routes for music
 router.delete("/:musicID", musicDelete);
 router.get("/", musicList);
