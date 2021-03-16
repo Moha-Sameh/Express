264821e22d50c2270db4591264e3ccb82666558b
Moha-Sameh <ms.slama01@hotmail.com>
2021-03-16 18:30:51 +0200

    Clean up code

--- src/controller/music.js
+++ src/controller/music.js
@@ -1,53 +1,46 @@
 const { music } = require("../db/models");
 
-exports.createMusic = async (req, res) => {
+exports.fetchMusic = async (id, next) => {
+  try {
+    const foundMusic = await music.findByPk(id);
+    return foundMusic;
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.musicUpdate = async (req, res, next) => {
+  try {
+    await req.music.update(req.body);
+    res.status(204).end();
+  } catch (err) {
+    next(error);
+  }
+};
+
+exports.musicDelete = async (req, res, next) => {
+  try {
+    await req.music.destroy();
+    res.status(204).end();
+  } catch (err) {
+    next(error);
+  }
+};
+
+exports.createMusic = async (req, res, next) => {
   try {
     const newMusic = await music.create(req.body);
     res.status(201).json(newMusic);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
-exports.newMusic = async (_, res) => {
+exports.newMusic = async (_, res, next) => {
   try {
     const musics = await music.findAll();
     res.json(musics);
   } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.musicUpdate = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const foundMusic = await music.findByPk(id);
-    if (foundMusic) {
-      await foundMusic.update(req.body);
-      res.status(204).end();
-    } else {
-      res
-        .status(404)
-        .json({ message: "Music ID must be wrong please try again" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.musicDelete = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const foundMusic = await music.findByPk(id);
-    if (foundMusic) {
-      await foundMusic.destroy();
-      res.status(204).end();
-    } else {
-      res
-        .status(404)
-        .json({ message: "Music ID must be wrong please try again" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
--- src/routes/music.js
+++ src/routes/music.js
@@ -4,9 +4,23 @@ const {
   createMusic,
   musicUpdate,
   musicDelete,
+  fetchMusic,
 } = require("../controller/music");
 
 const router = express.Router();
+
+// router using param
+router.param("id", async (req, res, next, id) => {
+  const music = await fetchMusic(id, next);
+  if (music) {
+    req.music = music;
+    next();
+  } else {
+    const err = new Error("Music ID must be wrong please try again");
+    err.status = 404;
+    next(err);
+  }
+});
 
 router.use(express.json());
 
